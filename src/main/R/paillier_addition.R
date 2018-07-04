f6fcb194086cfe8559735646dd70fa04de3098e4
michaelwalker <michael.walker@officespark.com>
2018-07-04 14:00:05 +0900

    python experiments

---
+++ src/main/R/paillier_addition.R
@@ -0,0 +1,122 @@
+library(tictoc)
+
+
+library(homomorpheR)
+keypair <<- PaillierKeyPair$new(modulusBits = 1024)
+pubkey <<- keypair$pubkey
+privkey  <<-keypair$getPrivateKey()
+largenum <<- 2^50
+zero  <<- pubkey$encrypt("0")
+
+# Convenience function
+zipf <<- function(x1,x2,x3) {
+    res = c()
+    for (i in 1:length(x1)) res = c(res,x1[i],x2[i],x3[i])
+    res
+}
+
+encodeFloat <<- function(x, e=0) {
+    x = x * 10^e
+    x1 = floor(x)
+    x2 = x - x1
+    x2 = floor(x2 * largenum)
+    exps = rep(e, length(x))
+    zipf(x1,x2,exps)
+}
+
+encrypt <<- function(z, e=0) {
+    y = encodeFloat(z, e)
+    res = rep(zero, length(y))
+    for (i in 1:length(y)) {
+        if (i %% 3 == 0) { res[i] = y[i] }
+        else { res[i] = pubkey$encrypt(as.character(y[i])) }
+    }
+    res
+}
+
+decrypt <<- function(en) {
+    uen = en
+    for (i in 1:length(uen)) {
+        if (i %% 3 != 0) uen[i] = privkey$decrypt(en[i])
+    }
+    # deal with negative results
+    for (i in 1:length(uen)) {
+        if (i %% 3 != 0 && # don't process exponents
+        uen[i] >= as.double(pubkey$n/3.0)) { uen[i] = uen[i] - pubkey$n }
+    }
+    res = c()
+    for (i in seq(1, length(uen), 3)) {
+        res = c(res,
+        (as.double(uen[i]) + as.double(uen[i+1]/(largenum))) / as.double(10^uen[i+2]))
+    }
+    return(res)
+}
+
+
+addenc <<- function(x, y) {
+    res = x
+    for (i in seq(1, length(x), 3)) {
+        if (x[i+2] != y[i+2]) { # if different exponent, make the exponents equal by multiplying
+            res[i+2] = max(x[i+2], y[i+2])
+            xdiff = res[i+2] - x[i+2]
+            ydiff = res[i+2] - y[i+2]
+            if (xdiff > 0) {
+                x[i] = pubkey$mult(x[i], 10^(xdiff))
+                x[i+1] = pubkey$mult(x[i+1], 10^(xdiff))
+            }
+            if (ydiff > 0) {
+                y[i] = pubkey$mult(y[i], 10^(ydiff))
+                y[i+1] = pubkey$mult(y[i+1], 10^(ydiff))
+            }
+        }
+        res[i] = pubkey$add(x[i], y[i])
+        res[i+1] = pubkey$add(x[i+1], y[i+1])
+    }
+    res
+}
+
+subenc <<- function(x, y) {
+    for (i in seq(1, length(x), 3)) {
+        y[i] = pubkey$mult(y[i], -1)
+        y[i+1] = pubkey$mult(y[i+1], -1)
+    }
+    addenc(x, y)
+}
+
+smultenc <<- function(x,y) {
+    prec = 5
+    yint = floor(y * 10^prec)
+    res = x
+    for (i in seq(1, length(x), 3)) {
+        res[i] = pubkey$mult(x[i], yint)
+        res[i+1] = pubkey$mult(x[i+1], yint)
+        res[i+2] = x[i+2] + prec
+    }
+    res
+}
+
+
+mult <<- function(inX, inY){
+    x = encrypt(inX)
+    y = encrypt(inY)
+    rand <- runif(2, -10, 10)
+    r1 = rand[1]
+    r2 = rand[2]
+    xdash = smultenc(x,r1)
+    ydash = smultenc(y,r2)
+    z = encrypt(decrypt(xdash)*decrypt(ydash))
+    smu = smultenc(z, 1/(r1*r2))
+    finish = decrypt(smu)
+    return(finish)
+}
+
+
+x1 <- sample.int(10000, 10000)
+tic()
+a = encrypt(x1)
+toc()
+
+print(mult(2.5,5.3))
+#for(i in 0 : 1000){
+#    print(mult(2.5,5.3))
+#}
---
+++ src/main/python/functionl_tests.py
@@ -0,0 +1,87 @@
+import math
+import itertools
+import phe.paillier as paillier
+pubkey, prikey = paillier.generate_paillier_keypair(n_length=1024)
+
+a = math.pi
+b = -a
+c = (1+math.pi*(10 ** -10))
+d = -c
+e = math.pi*(10 ** 17)
+f = -e
+
+tests = [a,b,c,d,e,f]
+combos = itertools.combinations(tests, 2)
+
+def formatDiff(inNumber):
+    return '%.2E' % inNumber
+
+def formatPad(inNumber):
+
+    if abs(inNumber) > 10e+17:
+        out = ("%+.3f"%inNumber)
+    else:
+        out = ("%+.20f"%inNumber).zfill(40)
+    return out
+
+def encDecTest():
+    print()
+
+    print("Encrypt-Decrypt TEST")
+    for x in tests:
+        ex = prikey.decrypt(pubkey.encrypt(x))
+        print(formatPad(x) + ", "+formatPad(ex) +", "+ formatDiff(x - ex))
+    return
+
+def additionTest():
+    print()
+    print("Addition TEST")
+    for x in combos:
+        added = x[0]+x[1]
+        e_x0 = pubkey.encrypt(x[0])
+        e_x1 = pubkey.encrypt(x[1])
+        e_added = e_x0 + e_x1
+        d_added = prikey.decrypt(e_added)
+        diff = added - d_added
+        print(formatPad(x[0]) + ", "+formatPad(x[1])+", "+formatDiff(diff)+", "+ formatPad(added)+ ","+formatPad(d_added))
+    return
+
+def multiplicationTest1():
+    print()
+    print("Multiplication TEST 1")
+    for x in combos:
+        multiplied = x[0]*x[1]
+        e_x0 = pubkey.encrypt(x[0])
+        e_x1 = pubkey.encrypt(x[1])
+        e_multiplied = e_x0 * x[1]
+        d_multiplied = prikey.decrypt(e_multiplied)
+        diff = multiplied - d_multiplied
+        print(formatPad(x[0]) + ", "+formatPad(x[1])+", "+formatDiff(diff)+", "+ formatPad(multiplied)+ ","+formatPad(d_multiplied))
+    return
+
+def multiplicationTest2():
+    print()
+    print("Multiplication TEST 2")
+    for x in combos:
+        multiplied = x[0]*x[1]
+        e_x0 = pubkey.encrypt(x[0])
+        e_x1 = pubkey.encrypt(x[1])
+        e_multiplied = e_x1 * x[0]
+        d_multiplied = prikey.decrypt(e_multiplied)
+        diff = multiplied - d_multiplied
+        print(formatPad(x[0]) + ", "+formatPad(x[1])+", "+formatDiff(diff)+", "+ formatPad(multiplied)+ ","+formatPad(d_multiplied))
+    return
+
+#encDecTest()
+#additionTest()
+#multiplicationTest1()
+multiplicationTest2()
+
+
+#The 53-bit significand precision gives from 15 to 17 significant decimal digits precision
+# (2−53 ≈ 1.11 × 10−16).
+# If a decimal string with at most 15 significant digits is converted
+# to IEEE 754 double-precision representation,
+#  and then converted back to a decimal string
+#  with the same number of digits, the final result should match the original string.
+
